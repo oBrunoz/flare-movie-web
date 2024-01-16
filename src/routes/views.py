b194025b8237f23408f69e641934fc98446fe90b
Bruno de Alencar Ferreira <bruno.ferreira@sipam.gov.br>
2024-01-16 18:07:25 -0300

    feat: new media route

--- src/routes/views.py
+++ src/routes/views.py
@@ -1,9 +1,15 @@
 from src import app
-from flask import render_template
-from src.api.config import getTrending
+from flask import render_template, stream_template
+from src.api.trending import getTrending
 
 @app.route('/')
 def index():
     trendingResponse = getTrending(media_type='all', time_window='week')
 
     return render_template('home.html', api=trendingResponse)
+
+@app.route('/media/<string:media_type>/<int:media_id>', methods=['GET'])
+def movie(media_type, media_id):
+    print(media_type, media_id)
+
+    return render_template('movies/movie.html')
