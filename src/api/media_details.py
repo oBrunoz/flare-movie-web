a6dbb351b70260a85ce2eb31b89ae4c6b41266ea
Bruno Alencar <alencarb98@gmail.com>
2024-01-22 00:41:44 -0300

    chore: added more parameters to getWatchProviders function

--- src/api/media_details.py
+++ src/api/media_details.py
@@ -3,17 +3,24 @@ from src.api.config import api_key, headers, format_release_date, convert_minute
 from random import randrange
 import requests
 
-def getWatchProviders(media_id:int, media_type:str):
+def getWatchProviders(media_id:int, media_type:str, country:str='BR'):
     URLS = {
-        'watch_providers': f'/{media_type}/{media_id}/watch/providers'
+        'watch_providers': f'/{media_type}/{media_id}/watch/providers?api_key='
     }
 
-    url_media_provider = f'{environ.get("GET_BASE_URL")}'
+    url_media_provider = f'{environ.get("GET_BASE_URL")}{URLS["watch_providers"]}{api_key}'
 
     try:
         response = requests.get(url=url_media_provider, headers=headers)
         response.raise_for_status()
         data = response.json()
+
+        if 'results' in data:
+            data_map = data['results'][country]
+
+            
+
+            return data['results'][country]
 
     except Exception as e:
         print(e)
@@ -53,41 +60,49 @@ def getMediaDetails(media_id: int, media_type: str, language: str = 'pt-BR') -> 
         'media_details': f'/{media_type}/{media_id}?language={language}&api_key='
     }
 
-    url_media_detail = f'{environ.get("GET_MEDIA_DETAILS")}{URLS["media_details"]}{api_key}'
-    response = requests.get(url=url_media_detail, headers=headers).json()
+    try:
+        url_media_detail = f'{environ.get("GET_BASE_URL")}{URLS["media_details"]}{api_key}'
+        response = requests.get(url=url_media_detail, headers=headers).json()
 
-    random_image_path, logo_image_path = getMediaImages(media_id, media_type, get_random_backdrop=True)
+        random_image_path, logo_image_path = getMediaImages(media_id, media_type, get_random_backdrop=True)
+        providers = getWatchProviders(media_id, media_type)
 
-    media_details = {
-        'adult': response.get('adult', False),
-        'backdrop_path': response.get('backdrop_path', None),
-        'logo_path': logo_image_path[0] if logo_image_path and len(logo_image_path) > 0 else None,
-        'belongs_to_collection': response.get('belongs_to_collection', None),
-        'budget': response.get('budget', 0),
-        'genres': [{'id': genre['id'], 'name': genre['name']} for genre in response.get('genres', [])],
-        'homepage': response.get('homepage', ''),
-        'id': response.get('id', 0),
-        'imdb_id': response.get('imdb_id', ''),
-        'original_language': response.get('original_language', ''),
-        'original_title': response.get('original_title', ''),
-        'overview': response.get('overview', ''),
-        'popularity': response.get('popularity', 0.0),
-        'poster_path': response.get('poster_path', ''),
-        'production_companies': [{'id': company['id'], 'logo_path': company['logo_path'], 'name': company['name'], 'origin_country': company['origin_country']} for company in response.get('production_companies', [])],
-        'production_countries': [{'iso_3166_1': country['iso_3166_1'], 'name': country['name']} for country in response.get('production_countries', [])],
-        'release_date': format_release_date(response.get('release_date', '')),
-        'revenue': response.get('revenue', 0),
-        'runtime': response.get('runtime', 0),
-        'runtime_hour': convert_minutes_to_hours(response.get('runtime')),
-        'spoken_languages': [{'english_name': lang['english_name'], 'iso_639_1': lang['iso_639_1'], 'name': lang['name']} for lang in response.get('spoken_languages', [])],
-        'status': response.get('status', ''),
-        'tagline': response.get('tagline', ''),
-        'title': response.get('title') if response.get('title') is not None else response.get('name'),
-        'video': response.get('video', False),
-        'vote_average': round(response.get('vote_average'), 1) if response.get('vote_average') else None,
-        'vote_count': response.get('vote_count', 0),
-    }
+        media_details = {
+            'adult': response.get('adult', False),
+            'backdrop_path': response.get('backdrop_path', None),
+            'logo_path': logo_image_path[0] if logo_image_path and len(logo_image_path) > 0 else None,
+            'belongs_to_collection': response.get('belongs_to_collection', None),
+            'budget': response.get('budget', 0),
+            'genres': [{'id': genre['id'], 'name': genre['name']} for genre in response.get('genres', [])],
+            'homepage': response.get('homepage', ''),
+            'id': response.get('id', 0),
+            'imdb_id': response.get('imdb_id', ''),
+            'original_language': response.get('original_language', ''),
+            'original_title': response.get('original_title', ''),
+            'overview': response.get('overview', ''),
+            'popularity': response.get('popularity', 0.0),
+            'poster_path': response.get('poster_path', ''),
+            'production_companies': [{'id': company['id'], 'logo_path': company['logo_path'], 'name': company['name'], 'origin_country': company['origin_country']} for company in response.get('production_companies', [])],
+            'production_countries': [{'iso_3166_1': country['iso_3166_1'], 'name': country['name']} for country in response.get('production_countries', [])],
+            'release_date': format_release_date(response.get('release_date', '')),
+            'watch_providers': providers,
+            'revenue': response.get('revenue', 0),
+            'runtime': response.get('runtime', 0),
+            'runtime_hour': convert_minutes_to_hours(response.get('runtime')),
+            'spoken_languages': [{'english_name': lang['english_name'], 'iso_639_1': lang['iso_639_1'], 'name': lang['name']} for lang in response.get('spoken_languages', [])],
+            'status': response.get('status', ''),
+            'tagline': response.get('tagline', ''),
+            'title': response.get('title') if response.get('title') is not None else response.get('name'),
+            'video': response.get('video', False),
+            'vote_average': round(response.get('vote_average'), 1) if response.get('vote_average') else None,
+            'vote_count': response.get('vote_count', 0),
+        }
+        print(media_details['watch_providers'])
+        return media_details
 
-    print('Media Details: ', media_details)
-
-    return media_details
+    except requests.RequestException as error:
+        raise error
+    except requests.HTTPError as error:
+        raise error
+    except Exception as e:
+        raise e
